2e4bd6636697d2f49704ca67a05b0734e56ab12e
Jigann <evgenij29082002@gmail.com>
2022-11-22 23:34:12 +0800

    добавил модальное окно

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import "../styles/authentication.css";
 import companyLogo from '../pics/enplus_2005.svg';
 import useValidation from '../hooks/useValidation';
 import useRedirection from '../hooks/useRedirection';
 import { validationCases, routes } from "../globalConstants";
+import Modal from "./modal";
 
 export default function Authentication() {
     const redirect = useRedirection();
     const { validate, update } = useValidation();
+    const [modalActive, setModalActive] = useState(false);
 
     useEffect(() => {
         const authForm = document.getElementById('Authentication');
@@ -20,9 +22,9 @@ export default function Authentication() {
             validationResults.push(validate(formInputs, validationCases.emptyFields));
 
             const firstFailedCase = validationResults.find(validationResult => validationResult.message != '');
-            update(formInputs, firstFailedCase !== undefined? firstFailedCase.inputs : []);
+            update(formInputs, firstFailedCase !== undefined ? firstFailedCase.inputs : []);
 
-            if (firstFailedCase === undefined){
+            if (firstFailedCase === undefined) {
                 // Тут обращение к бекенду
                 redirect(routes.main);
             }
@@ -37,8 +39,11 @@ export default function Authentication() {
             <div className="info d-flex flex-column pt-5 pe-2 ps-2">
                 <input className="input-placeholder p-2" type="text" placeholder="Логин" autoComplete="off"></input>
                 <input className="input-placeholder p-2" type="password" placeholder="Пароль" autoComplete="off"></input>
-                <button type="submit">Войти</button>
+                <button type="submit" onClick={() => setModalActive(true)}>Войти</button>
             </div>
+            <Modal active={modalActive} setActive={setModalActive}>
+                Возникла ошибка! Проверьте введенные данные!
+            </Modal>
         </form>
     )
 }
