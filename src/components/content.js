aa393da5cac46a68d95616e2cc9af3e24169836f
Евгений Афанасьев <evgenij29082002@gmail.com>
2023-03-02 09:54:00 +0800

    Сверстал первую версию headerа

--- src/components/content.js
+++ src/components/content.js
@@ -1,6 +1,9 @@
+import Header from './Header';
+
 export default function Content() {
     return (
         <div id="Content" className="d-flex flex-column">
+            <Header/>
             <span className="text-white">Content</span>
         </div>
     )
