648e1705881f614b64125909c43ab2d6ba1653f8
ZaharDomolego <domolegozahar@gmail.com>
2023-05-17 19:54:58 +0800

    Поменял надпись в подвале

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
         <div id="Footer" className="d-flex mt-auto me-auto ms-auto flex-column p-1">
             <img className="logo-nav d-flex mb-3 mt-5" src={`/pics/enplus_2005_${isDarkTheme? 'dark' : 'light'}.svg`}
                  alt="company logo"></img>
-            <span className="d-flex align-items-center text-center mb-3">WEB-интерфейс для 1С систем <br /> 2023 </span>
+            <span className="d-flex align-items-center text-center mb-3">WEB-интерфейс АСУДЗ <br /> 2023 </span>
         </div>
     )
 }
